3ebd2af1d5f54d6c89c8ae1afe9c49cad26fedf4
Gayathri0017 <gayathri4866@gmail.com>
2025-04-02 10:43:41 +0530

    commit

--- saucedemo_cu/src/test/java/Runner/LoginRunnerFile.java
+++ saucedemo_cu/src/test/java/Runner/LoginRunnerFile.java
@@ -1,7 +1,15 @@
 package Runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(plugin={"pretty","html:target/cucumber-reports/Cucumber.html"},features="src\\test\\resources\\features\\Login.feature",glue="StepDefinition")
-public class LoginRunnerFile extends AbstractTestNGCucumberTests{
+@CucumberOptions(
+	    plugin = {
+	        "pretty",
+	        "html:target/cucumber-reports/Cucumber.html",
+	        "json:target/cucumber-reports/Cucumber.json"
+	    },
+	    features ="src\\test\\resources\\features\\Login.feature",
+	    glue ="StepDefinition"
+	)
+public class LoginRunnerFile extends AbstractTestNGCucumberTests {
 	
 }
